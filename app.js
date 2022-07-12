89bc877dabf35ebe48e008d4edd964ad61fe3990
b3ssle <hi@b3ssle.be>
2022-07-12 22:48:27 +0900

    project start

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express()
+const port = 3000
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+// starts the express server and listening for connections.
+app.listen(port, () => {
+  console.log(`Express app listening on port ${port}.`)
+})
