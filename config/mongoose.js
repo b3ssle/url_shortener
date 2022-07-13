0a18785331e497356ee86985766f56fd130abc39
b3ssle <hi@b3ssle.be>
2022-07-13 21:34:44 +0900

    Fix views & layouts

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 
-mongoose.connect('mongodb://localhost/url_shortener', {
+mongoose.connect("mongodb://localhost/url_shortener", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
 
 const db = mongoose.connection
 
-db.on('error', () => {
-  console.log('mongodb error!')
+db.on("error", () => {
+  console.log("mongodb error!")
 })
 
-db.once('open', () => {
-  console.log('mongodb connected!')
+db.once("open", () => {
+  console.log("mongodb connected!")
 })
 
 module.exports = db
