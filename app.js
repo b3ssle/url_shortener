f9fe1ce09314ed9d965394757ae6be9502c87d6f
b3ssle <hi@b3ssle.be>
2022-07-20 20:26:47 +0900

    Add Error.hbs and fix app.js

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const URL = require("./models/URL")
 const app = express()
 const port = 3000
 
+app.use(express.urlencoded({ extended: true }))
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }))
 app.set("view engine", "handlebars")
 app.use(express.static("public"))
@@ -21,18 +23,36 @@ app.post("/", (req, res) => {
 
   URL.findOne({ originalURL: req.body.url })
     .then(data => {
-      if (data) {
+      if (data) { // 比對網址，如果網址一樣，則回傳之前建立過的縮址
         return data
-      } else {
+      } else { // 比對網址，如果網址不一樣，則產生新的縮址
         return URL.create({ shortURL: short, originalURL: url})
       }
     })
     .then(data =>
       res.render("results", {
-        originalURL: req.headers.origin,
+        origin: req.headers.origin,
+        originalURL: url,
         shortURL: data.shortURL,
       })
     )
+    .catch(error => console.error(error))
+})
+
+app.get("/:shortURL", (req, res) => {
+  const { shortURL } = req.params
+
+  URL.findOne({ shortURL })
+    .then(data => {
+      if (!data) {
+        return res.render("error", {
+          errorMsg: "Can't found the URL",
+          errorURL: req.headers.host + "/" + shortURL,
+        })
+      }
+
+      res.redirect(data.originalURL)
+    })
     .catch(error => console.error(error))
 })
 
